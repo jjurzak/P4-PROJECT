eb50063fc3bcfbd239c4c4f2c139afc7e83b78ef
sathek <xdkuba6@gmail.com>
2023-03-09 10:38:47 +0100

    Created CSV reader, basic display function. Excel colums mapped from A to H.

---
+++ ProjektP4/Hotels.cs
@@ -0,0 +1,28 @@
+﻿using CsvHelper.Configuration;
+namespace ProjektP4;
+
+public class Hotels
+{
+    public int Lp { get; set; }
+    public string NazwaWlasnna { get; set; }
+    public string Telefon { get; set; }
+    public string Email { get; set; }
+    public string CharakterUslug { get; set; }
+    public string KategoriaObiektu { get; set; }
+    public string RodzajObiektu { get; set; }
+    public string Adres { get; set; }
+}
+public sealed class HotelsMap : ClassMap<Hotels>
+{
+    public HotelsMap()
+    {
+        Map(m => m.Lp).Index(0);
+        Map(m => m.NazwaWlasnna).Index(1);
+        Map(m => m.Telefon).Index(2);
+        Map(m => m.Email).Index(3);
+        Map(m => m.CharakterUslug).Index(4);
+        Map(m => m.KategoriaObiektu).Index(5);
+        Map(m => m.RodzajObiektu).Index(6);
+        Map(m => m.Adres).Index(7);
+    }
+}
---
+++ ProjektP4/Program.cs
@@ -0,0 +1,38 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Globalization;
+
+
+namespace ProjektP4
+{
+    class Program
+    {
+        static void DisplayHotels(Hotels h)
+        {
+            Console.WriteLine($"Lp.: {h.Lp} Nazwa własna: {h.NazwaWlasnna} Telefon: {h.Telefon} Email: {h.Email} Charakter usług:" +
+                              $" {h.CharakterUslug} Kategoria obiektu: {h.KategoriaObiektu} Rodzaj obiektu: {h.RodzajObiektu} Adres: {h.Adres}");
+        }
+        
+        static void Main(string[] args)
+        {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ";"
+            };
+            
+            using (var reader = new StreamReader("hotele.csv"))
+            using (var csv = new CsvReader(reader, config))
+            {
+                csv.Context.RegisterClassMap<HotelsMap>();
+                var records = csv.GetRecords<Hotels>().ToList();
+                foreach (var h in records)
+                {
+                    DisplayHotels(h);
+                }
+            }
+        }
+    }
+
+    
+}
